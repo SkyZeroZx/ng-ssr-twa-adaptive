56e2631154828d681836f1b477d55b82fe7772c4
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2025-08-11 15:54:25 -0500

    chore : update app base

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,20 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  inject,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContextService } from '@/services/context';
-import { TuiRoot } from "@taiga-ui/core";
+import { TuiRoot } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,  ],
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class App {
   constructor() {
