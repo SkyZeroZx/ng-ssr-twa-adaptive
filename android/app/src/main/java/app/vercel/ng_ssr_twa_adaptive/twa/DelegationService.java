7e4f9d50951b70d3993330b32cdccb0c13259c60
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2025-08-31 16:13:52 -0500

    chore: Add apk release output from bubblewrap in android folder

---
+++ android/app/src/main/java/app/vercel/ng_ssr_twa_adaptive/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.ng_ssr_twa_adaptive.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
